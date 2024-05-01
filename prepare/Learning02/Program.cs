2fc82024fe638951bd8bbf70a9b0374300210a7d
Albert Conceição <developer.albert@outlook.com>
2024-05-01 10:22:47 -0300

    feat: job and resume class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2028;
+        job1._endYear = 2035;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Senior Software Engineer";
+        job2._startYear = 2035;
+        job2._endYear = 2060;
+
+        Resume myResume = new Resume();
+
+        myResume._name = "Albert Conceição";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+   
     }
 }
