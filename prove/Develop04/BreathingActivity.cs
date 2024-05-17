c58b8617a52f4cec765ec994faec892c9c3c66a6
Albert Conceição <developer.albert@outlook.com>
2024-05-16 23:23:22 -0300

    feat: updating prove 4

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -13,22 +13,19 @@ public class BreathingActivity : Activity
         ShowSpinner(5);
         Console.Clear();
         int totalDuration = _duration;
-        int cycleTime = 4 + 6; // Total time for one cycle of breath in and breath out
+        int cycleTime = 4 + 6; 
         int fullCycles = totalDuration / cycleTime;
         int remainingTime = totalDuration % cycleTime;
 
         for (int i = 0; i < fullCycles; i++)
         {
-            // Breath in for 4 seconds
             Console.Write("Breath in... ");
             ShowCountDown(4);
 
-            // Breath out for 6 seconds
             Console.Write("Now breath out... ");
             ShowCountDown(6);
         }
 
-        // Handle remaining time after full cycles
         if (remainingTime > 0)
         {
             if (remainingTime <= 4)
